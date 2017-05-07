c07dc39e303b47b1ab5ae67063d927794b08912e
Théo Giovanna <theo.giovanna@gmail.com>
2017-05-07 18:47:10 +0200

    Added game design.

--- src/Battleship/gui/MainView.java
+++ src/Battleship/gui/MainView.java
@@ -8,70 +8,21 @@ package Battleship.gui;
  * Created by arthurdeschamps on 05.05.17.
  */
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import Battleship.App.*;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JPanel;
+import javax.swing.AbstractAction;
+import javax.swing.*;
+import java.awt.event.*;
 
+import net.miginfocom.swing.MigLayout;
 
-
-public class MainView {
-
-    public MainView() {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-                }
-
-                JFrame frame = new JFrame("Bataille navale");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new BorderLayout());
-                frame.add(new DrawGrid());
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-            }
-        });
-    }
-}
-
-/*
-
-package Battleship.gui;
-
-
-
-        import Battleship.App.*;
-
-        import javax.swing.*;
-        import java.awt.*;
-        import java.awt.event.*;
-
-
-<<<<<<< HEAD
-
-=======
-
-/** VERSION 2 - BUTTONS */
-
-/**
->>>>>>> 392ffec2cc2d3be185bb63b55a96d476d2f95015
 public class MainView extends JFrame{
 
-
-    public MainView() {
-        super();
-        build();
-    }
-
-
-
-    private void build(){ //Set windows
-
+    public MainView(){
+        super("MigLayout Basic");
         setTitle("Battleship - WarZone");
         setSize(1200,800);
         setLocationRelativeTo(null); //Center
@@ -81,16 +32,12 @@ public class MainView extends JFrame{
         setVisible(true);
     }
 
-
-
     public class Abandon extends AbstractAction { //Actions for buttons
         public Abandon(String texte) {
             super(texte);
         }
 
         public void actionPerformed(ActionEvent e) {
-
-
 
             if (JOptionPane.showConfirmDialog(null, "Are you sure you wanna quit like a lil' coward?", "WARNING",
                     JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
@@ -111,25 +58,38 @@ public class MainView extends JFrame{
         }
     }
 
+    //Designing the board
+
     private JPanel button(){ //Set buttons in pannel
 
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        panel.setLayout(new MigLayout("debug, fillx", "[][grow][]"));
 
         JButton b1 = new JButton(new Start("Start"));
-        panel.add(b1);
+        panel.add(b1, "growx, w " + this.getWidth()/5);
+
+        JLabel oppGrid = new JLabel("Opponent grid should be here");
+        panel.add(oppGrid, "span 2 3 " + this.getWidth()/5 + ", grow, wrap, h " + this.getWidth()/4);
 
         JButton b2 = new JButton(new Abandon("Abandon"));
-        panel.add(b2);
+        panel.add(b2, "growx, wrap, w " + this.getWidth()/5);
+
+        JLabel boatSelect = new JLabel("Boat orientation select should be here");
+        panel.add(boatSelect, "w " + this.getWidth()/5 + ", grow, wrap, h " + this.getWidth()/4);
+
+        JLabel userGrid = new JLabel("Boat select should be here");
+        panel.add(userGrid, "span 1 2 " + this.getWidth()/5 + ", grow, wrap, h " + this.getWidth()/3);
+
+        JLabel boatOrientation = new JLabel("UserGrid should be here");
+        panel.add(boatOrientation, "w " + this.getWidth()/5 + ", grow, wrap, h " + this.getWidth()/3);
 
         return panel;
     }
 
 }
- **/
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 392ffec2cc2d3be185bb63b55a96d476d2f95015
+
+
+
